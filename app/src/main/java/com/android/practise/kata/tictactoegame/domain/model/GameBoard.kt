725aebe7f3457c8a470b72a72d3e2e6bce9e978f
coder <roshandevops@gmail.com>
2025-12-13 13:58:19 +0100

    refactor(GameBoard) : make Columns and Rows more readable
    - extract complex func into smaller

--- app/src/main/java/com/android/practise/kata/tictactoegame/domain/model/GameBoard.kt
+++ app/src/main/java/com/android/practise/kata/tictactoegame/domain/model/GameBoard.kt
@@ -5,7 +5,17 @@ import javax.inject.Inject
 
 internal class GameBoard @Inject constructor() {
 
-     private val size: Int  = BOARD_SIZE
+    private val size: Int  = BOARD_SIZE
+
+    companion object {
+        private const val MIN_INDEX = 0
+        private const val FIRST_ROW = 0
+        private const val SECOND_ROW = 1
+        private const val THIRD_ROW = 2
+        private const val FIRST_COL = 0
+        private const val SECOND_COL = 1
+        private const val THIRD_COL = 2
+    }
 
     var winner: Player = Player.NONE
         private set
@@ -18,7 +28,7 @@ internal class GameBoard @Inject constructor() {
     }
 
     fun makeMove(row: Int, col: Int, player: Player): Boolean {
-        if (row !in 0 until size || col !in 0 until size) {
+        if (row !in MIN_INDEX until size || col !in MIN_INDEX until size) {
             return false
         }
         if (board[row][col] != Player.NONE) {
@@ -29,32 +39,46 @@ internal class GameBoard @Inject constructor() {
     }
 
     fun isGameWon(): Boolean {
+        return hasHorizontalWin() || hasVerticalWin() || hasDiagonalWin()
+    }
+
+    private fun hasHorizontalWin(): Boolean {
         for(row in 0 until size) {
-            if(board[row][0] != Player.NONE &&
-                board[row][0] == board[row][1] &&
-                board[row][1] == board[row][2]) {
+            if(board[row][FIRST_COL] != Player.NONE &&
+                board[row][FIRST_COL] == board[row][SECOND_COL] &&
+                board[row][SECOND_COL] == board[row][THIRD_COL]) {
                 winner = board[row][0]
                 return true
             }
         }
+        return false
+    }
+
+    private fun hasVerticalWin(): Boolean {
         for(col in 0 until size) {
-            if(board[0][col] != Player.NONE &&
-                board[0][col] == board[1][col] &&
-                board[1][col] == board[2][col]) {
+            if(board[FIRST_ROW][col] != Player.NONE &&
+                board[FIRST_ROW][col] == board[SECOND_ROW][col] &&
+                board[SECOND_ROW][col] == board[THIRD_ROW][col]) {
                 winner = board[0][col]
                 return true
             }
         }
-        if(board[0][0] != Player.NONE &&
-            board[0][0] == board[1][1] &&
-            board[1][1] == board[2][2]) {
-            winner = board[0][0]
+        return false
+    }
+
+
+    private fun hasDiagonalWin(): Boolean {
+        if (board[FIRST_ROW][FIRST_COL] != Player.NONE &&
+            board[FIRST_ROW][FIRST_COL] == board[SECOND_ROW][SECOND_COL] &&
+            board[SECOND_ROW][SECOND_COL] == board[THIRD_ROW][THIRD_COL]) {
+            winner = board[FIRST_ROW][FIRST_COL]
             return true
         }
-        if(board[0][2] != Player.NONE &&
-            board[0][2] == board[1][1] &&
-            board[1][1] == board[2][0]) {
-            winner = board[0][2]
+
+        if (board[FIRST_ROW][THIRD_COL] != Player.NONE &&
+            board[FIRST_ROW][THIRD_COL] == board[SECOND_ROW][SECOND_COL] &&
+            board[SECOND_ROW][SECOND_COL] == board[THIRD_ROW][FIRST_COL]) {
+            winner = board[FIRST_ROW][THIRD_COL]
             return true
         }
         return false
